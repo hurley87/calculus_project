d4ff8ce9466de87b92278cb999cfe87c26050757
davidhurley <dhurls99@gmail.com>
2014-06-14 19:13:47 -0400

    style changes to form page and jquery to order the questions

--- public/calc.js
+++ public/calc.js
@@ -1,7 +1,5 @@
-
+		// code for the timer
     var totalSeconds = 0;
-    
-
     function setTime()
     {
         ++totalSeconds;
@@ -24,17 +22,44 @@
 
 
 $(document).ready(function() {
-		
-		$('#start').on('click', function() {
-			setInterval(setTime, 1000);
-		});
- 		$('#submit').on('click', function() {
-					$('#timer').val(totalSeconds);
- 		});	
 
- 		$('.q2').hide();
+		// hide everything off the start
+		$('.q1').hide();
+ 		$('.q2').hide(); 
  		$('.q3').hide();
  		$('.q4').hide();
  		$('.q5').hide();
+ 		$('#stopwatch').hide();
+ 		$('#submit').hide();
+
+ 		// focus cursor on name and show timers when user enters name
+ 		$('#name').focus();
+ 		$("#name").change(function() {
+ 			$('.name').hide(1000)
+ 			$('#stopwatch').show(2000);
+ 		});
 	
+			// when user clicks start the timer starts and the 
+			// first question is shown
+		$('#start').on('click', function() {
+			setInterval(setTime, 1000);
+			$('.q1').fadeIn(1000);
+		});
+
+
+
+ 		$('#submit').on('click', function() {
+					$('#timer').val(totalSeconds);
+ 		});	
+ 		
+
+ 		$("#calc_form").submit(function(e){
+    		return false;
+		});
+
+		$("#start").on("click", function() {
+					$(this).fadeOut();
+		});
+
+ 		
 });
