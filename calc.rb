83d7ba2795c4259ce612ff831b7a970ad4bca1d6
davidhurley <dhurls99@gmail.com>
2014-06-14 15:40:48 -0400

    start of calculus game

---
+++ calc.rb
@@ -0,0 +1,42 @@
+require 'sinatra'
+require 'data_mapper'
+
+DataMapper.setup(:default, "sqlite3:database.sqlite3")
+
+class Record
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String
+  property :time, Float
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
+
+file = File.open("questions.txt").read
+q_arr = []
+a_arr = []
+count = 1
+file.each_line do |line|
+	count += 1
+	q_arr << line.gsub("\n", "") if count % 2 == 0
+	a_arr << line.gsub("\n", "") if count % 2 != 0
+end
+
+
+get '/' do
+	@questions = q_arr
+	@answers = a_arr
+	@rand_var = rand(@questions.length)
+  erb :calculus
+end
+
+post '/' do
+	@record = Record.create(
+			:time => params[:time]
+		) 
+		redirect to('/')
+end
+# database - record user and time
+
---
+++ public/calc.js
@@ -0,0 +1,37 @@
+
+    var totalSeconds = 0;
+    
+
+    function setTime()
+    {
+        ++totalSeconds;
+        $("#minutes").text(pad(totalSeconds%60));
+        $("#seconds").text(pad(parseInt(totalSeconds/60)));
+    }
+
+    function pad(val)
+    {
+        var valString = val + "";
+        if(valString.length < 2)
+        {
+            return "0" + valString;
+        }
+        else
+        {
+            return valString;
+        }
+    }
+
+
+
+
+$(document).ready(function() {
+		
+		$('#start').on('click', function() {
+			setInterval(setTime, 1000);
+		});
+ 		$('#submit').on('click', function() {
+					$('#timer').val(totalSeconds);
+ 		});	
+	
+});
